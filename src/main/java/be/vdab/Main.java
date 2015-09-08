ac3ac6af6a95503a588f2e55cd0b1652ecd063d0
chris <chrisnevelsteen@gmail.com>
2015-09-08 14:37:36 +0200

    tot p29

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -9,31 +9,40 @@ import be.vdab.services.EuroService;
 public class Main {
 	public static void main(String[] args) {
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"container.xml")) {	
+				"restclient.xml", "services.xml")) {
+/*			System.out.println(context.getBean(ClassA.class)
+					.getTelefoonNrHelpDesk());
 			ClassA objectA = context.getBean(ClassA.class);
+			System.out.println(context.getBean("classA", ClassA.class)
+					.getBoodschap());
+			System.out.println(context.getBean("classB", ClassB.class)
+					.getBoodschap());
 			System.out.println(objectA.getKoersenURL());
 			System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
-			System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
+			System.out.println("Bestand bestaat: "
+					+ objectA.getImportData().exists());
 			System.out.println(objectA.getDefaultBladRichting());
-			
+
 			ClassB objectB = context.getBean(ClassB.class);
 			System.out.println(objectB.getTelefoonNrHelpDesk());
 			System.out.println(objectB.getAantalPogingenUpdateKlant());
-			
+
 			System.out.println(context.getBean(HelpdeskMedewerkers.class));
-			
-			System.out.println(context.getBean(ClassA.class).getWebMasterEMailAdres());
-			System.out.println(context.getBean(ClassA.class).getWebsiteGestart());
-			
-			context.getBean("teller1", Teller.class).verhoog(); 
-			context.getBean("teller1", Teller.class).verhoog(); 
-			Teller teller2 = context.getBean("teller2", Teller.class); 
-			teller2.verhoog(); 
-			context.getBean("teller2", Teller.class).verhoog();
-			
-			System.out.println(
-					context.getBean(EuroService.class).naarDollar(BigDecimal.valueOf(2)));
+
+			System.out.println(context.getBean(ClassA.class)
+					.getWebMasterEMailAdres());
+			System.out.println(context.getBean(ClassA.class)
+					.getWebsiteGestart());
+
+			context.getBean("teller1", Teller.class).verhoog();
+			context.getBean("teller1", Teller.class).verhoog();
+			Teller teller2 = context.getBean("teller2", Teller.class);
+			teller2.verhoog();
+			context.getBean("teller2", Teller.class).verhoog();*/
+
+			System.out.println(context.getBean(EuroService.class).naarDollar(
+					BigDecimal.valueOf(2)));
 		}
-			
+
 	}
 }
